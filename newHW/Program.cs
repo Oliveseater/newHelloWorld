f0b98d3ec9c0faa0471d3a5b62db519141ae0efb
Oliveseater <nslivnicina@gmail.com>
2022-01-16 18:03:19 +0500

    message

---
+++ newHW/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newHW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b;
+            Console.WriteLine("Введите А");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите В");
+            //b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(a + b);
+            //Console.WriteLine(a - b);
+            //Console.WriteLine(a * b);
+            //Console.WriteLine(a / b);
+            //Console.ReadKey();
+        }
+    }
+}
